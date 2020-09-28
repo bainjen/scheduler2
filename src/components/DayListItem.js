e03f05f90307e3477728b68baec733efbfa653d0
Jennifer Bain <jenniferbain@jennifers-air.lan>
2020-09-28 17:38:13 -0600

    create function formatSpots in DayListItem to render a string stating number of spots open in a day

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,6 +2,16 @@ import React from "react";
 import classnames from "classnames";
 import "components/DayListItem.scss";
 
+const formatSpots = (spots) => {
+  if (spots === 0) {
+    return 'no spots remaining'
+  } else if (spots === 1) {
+    return '1 spot remaining'
+  } else {
+    return `${spots} spots remaining`
+  }
+}
+
 export default function DayListItem(props) {
 
   const dayClass = classnames("day-list__item", {
@@ -9,6 +19,7 @@ export default function DayListItem(props) {
     "day-list__item--full": props.spots === 0
   });
 
+  const spotsMessage = formatSpots(props.spots);
 
   return (
     <li
@@ -16,7 +27,7 @@ export default function DayListItem(props) {
       onClick={() => props.setDay(props.name)}
     >
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{spotsMessage}</h3>
     </li>
   );
 }
