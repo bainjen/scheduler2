0b21d824bd499931118bc78cb6a681d37a109fe7
Jennifer Bain <jenniferbain@Jennifers-MacBook-Air.local>
2020-09-29 10:25:09 -0600

    setup files for new InterviewerList components

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import classnames from "classnames"
+import "components/InterviewerList.scss";
+
+
+export default function InterviewerList(props) {
+  // const buttonClass = classnames("button", {
+  //   "button--confirm": props.confirm,
+  //   "button--danger": props.danger
+  // });
+
+  return (
+    <InterviewerList>
+    {/* //   className={buttonClass}
+    //   onClick={props.onClick}
+    //   disabled={props.disabled}
+    // >
+    //   {props.children} */}
+    </InterviewerList>
+  );
+}
