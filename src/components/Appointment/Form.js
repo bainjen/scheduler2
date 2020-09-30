38b11351d32c62ce433f2ee3fdb451967b04a44f
Jennifer Bain <jenniferbain@Jennifers-MacBook-Air.local>
2020-09-29 20:26:31 -0600

    add useState to form.js and update the setInterviewer function to be definec int the interviwer listitem rather than interviewer list.

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -3,27 +3,29 @@ import Button from "../Button";
 import InterviewerList from "../InterviewerList";
 
 export default function Form(props) {
-  // const [interviewer, setInterviewer] = useState(2);
-  const setInterviewer = () => console.log('hello')
-  // const [name, setName] = useState("");
+  const [interviewer, setInterviewer] = useState(3);
+  const [name, setName] = useState("");
 
-  const placeholderVal = props.name ? props.name : "Enter Student Name"
+  // const setInterviewer = () => console.log('hello')
+  // const placeholderVal = props.name ? props.name : "Enter Student Name"
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
         <form autoComplete="off">
           <input
+            value={name}
+            onChange={(event) => setName(event.target.value)}
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
-            placeholder={placeholderVal}
+            placeholder="Enter student name"
           /*
             This must be a controlled component
           */
           />
         </form>
-        <InterviewerList interviewers={props.interviewers} interviewer={props.interviewer} onChange={setInterviewer} />
+        <InterviewerList interviewers={props.interviewers} interviewer={interviewer} setInterviewer={setInterviewer} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -9,10 +9,11 @@ export default function InterviewerList(props) {
   const interviewerListItems = props.interviewers.map((interviewer) => {
     return (
       <InterviewerListItem key={interviewer.id}
+        id={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
         selected={interviewer.id === props.interviewer}
-        setInterviewer={event => props.setInterviewer(interviewer.id)}
+        setInterviewer={props.setInterviewer}
       />
     )
   })
