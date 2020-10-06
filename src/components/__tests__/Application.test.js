3684d603dc83862a95d0d81b19b8255239b23164
Jennifer Bain <jennifer.fleming.bain@gmail.com>
2020-10-06 15:15:39 -0600

    update format of the tests for Application component to include await

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,11 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import {
+  render,
+  cleanup,
+  waitForElement,
+  fireEvent,
+} from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -11,6 +16,7 @@ afterEach(cleanup);
 // });
 
 describe("Application", () => {
+  //uses raw promise syntax
   it("defaults to Monday and changes the schedule when a new day is selected", () => {
     const { getByText } = render(<Application />);
 
@@ -18,6 +24,17 @@ describe("Application", () => {
       fireEvent.click(getByText("Tuesday"));
       expect(getByText("Leopold Silvers")).toBeInTheDocument();
     });
+  });
+
+  //uses async/await syntax
+  it("changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
+
+    await waitForElement(() => getByText("Monday"));
+
+    fireEvent.click(getByText("Tuesday"));
+
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
 });
 
