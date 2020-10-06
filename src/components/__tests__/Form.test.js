e73c0814eacee6243898c5b2292861394ec18e35
Jennifer Bain <jennifer.fleming.bain@gmail.com>
2020-10-05 19:34:47 -0600

    update all form tests to match those on compass

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -1,6 +1,8 @@
 import React from "react";
 
-import { render, cleanup, fireEvent } from "@testing-library/react";
+import { fireEvent } from "@testing-library/react";
+
+import { render, cleanup } from "@testing-library/react";
 
 import Form from "components/Appointment/Form";
 
@@ -24,29 +26,17 @@ describe("Form", () => {
 
   it("renders with initial student name", () => {
     const { getByTestId } = render(
-      <Form name="Lydia Miller-Jones" interviewers={interviewers} />
+      <Form interviewers={interviewers} name="Lydia Miller-Jones" />
     );
     expect(getByTestId("student-name-input")).toHaveValue("Lydia Miller-Jones");
   });
 
-  it("renders with initial student name", () => {
-    const { getByPlaceholderText } = render(
-      <Form name="Lydia Miller-Jones" interviewers={interviewers} />
-    );
-    expect(getByPlaceholderText("Enter Student Name")).toHaveValue(
-      "Lydia Miller-Jones"
-    );
-  });
-
   it("validates that the student name is not blank", () => {
-    //create mock save fn
     const onSave = jest.fn();
-    //render Form with interviewers and the onSave mock function passed as an onSave prop, the name prop should be blank or undefined
     const { getByText } = render(
       <Form interviewers={interviewers} onSave={onSave} />
     );
 
-    //click the save button
     fireEvent.click(getByText("Save"));
 
     expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
@@ -104,21 +94,6 @@ describe("Form", () => {
 });
 
 //We have improved the test by making it resemble the way we use our software. Instead of initializing the component with a default value, we simulate user interaction. This test is more accurate, and as a bonus, we are getting better test coverage.
-
-//   it("submits the name entered by the user", () => {
-//     const onSave = jest.fn();
-//     const { getByText, getByPlaceholderText } = render(
-//       <Form interviewers={interviewers} onSave={onSave} />
-//     );
-
-//     const input = getByPlaceholderText("Enter Student Name");
-
-//     fireEvent.change(input, { target: { value: "Lydia Miller-Jones" } });
-//     fireEvent.click(getByText("Save"));
-
-//     expect(onSave).toHaveBeenCalledTimes(1);
-//     expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
-//   });
 
 //   When we use getByText we should be confident that the element exists. If it does not exist, then it throws an error instead. We can test for the absence of something by using queryByText and checking that the value is null.
 // It is a case insensitive regular expression. This search is more general than the absolute string "Student name cannot be blank".
