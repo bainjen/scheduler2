124c3329c0ebbf74df2f79badd9ea7e6e4e81bfe
Jennifer Bain <jennifer.fleming.bain@gmail.com>
2020-10-06 14:52:20 -0600

    delete an unused test file because it was empty. create mock axios data in __mock__ foler and use this data to test Application component

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,29 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
+// xit("renders without crashing", () => {
+//   render(<Application />);
+// });
+
+describe("Application", () => {
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { getByText } = render(<Application />);
+
+    return waitForElement(() => getByText("Monday")).then(() => {
+      fireEvent.click(getByText("Tuesday"));
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    });
+  });
 });
+
+// We will write a test to confirm that the scheduler can load data.
+// We will write an asynchronous test that waits for a component to update before proceeding.
+// We will use containers to find specific DOM nodes.
+// We will chain promises to handle asynchronous testing.
+// We will override mock implementations for specific tests.
+// We will use setup and teardown functions provided by Jest to perform common tasks.
--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -26,7 +26,7 @@ export default function useApplicationData() {
     const days = mutateDaysAtSingleDay(state, id, appointments);
 
     //edit interview data
-    return axios.put(`http://localhost:8001/api/appointments/${id}`, {
+    return axios.put(`/api/appointments/${id}`, {
       interview: appointment.interview
     })
       .then(resp => {
@@ -51,7 +51,7 @@ export default function useApplicationData() {
     };
     const days = mutateDaysAtSingleDay(state, id, appointments);
 
-    return axios.delete(`http://localhost:8001/api/appointments/${id}`)
+    return axios.delete(`/api/appointments/${id}`)
       .then(resp => {
 
         setState({
@@ -63,9 +63,9 @@ export default function useApplicationData() {
   }
   useEffect(() => {
     Promise.all([
-      axios.get('http://localhost:8001/api/days'),
-      axios.get('http://localhost:8001/api/appointments'),
-      axios.get('http://localhost:8001/api/interviewers')
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
     ]).then((all) => {
 
       const days = all[0].data;
