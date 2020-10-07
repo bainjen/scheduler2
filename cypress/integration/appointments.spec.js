55db8455e1e23431d8f3fdfde4fbfcf6dfc84dd2
Jennifer Bain <jennifer.fleming.bain@gmail.com>
2020-10-07 16:38:39 -0600

    implement cypress testing to assure the cancel interview function works

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -30,7 +30,22 @@ describe("Appointments", () => {
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Tori Malcolm");
   })
+
+  it("should cancel an interview", () => {
+    cy.get("[alt=Delete]").first().click({ force: true });
+    cy.contains("Confirm").click(); 
+    cy.contains("deleting").should("exist"); 
+    cy.contains("deleting").should("not.exist");
+    cy.contains(".appointment__card--show", "Archie Cohen")
+    .should("not.exist");
+    // cy.get("[data-testid=student-name-input]").clear().type("Lydia Miller-Jones");
+    // cy.get("[alt='Tori Malcolm']").click();
+    // cy.contains("Save").click();
+    // cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    // cy.contains(".appointment__card--show", "Tori Malcolm");
+  })
 });
+
 // cy.contains("[data-testid=day]", "Tuesday")
 //   .click()
 //   .should("have.class", "day-list__item--selected");
